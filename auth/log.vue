c61e887acb2335843507a0584d26d29dbce924fc
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-04-17 22:52:31 +0530

    Create log.vue

---
+++ auth/log.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input type="text" v-model="username" placeholder="Username">
+      <input type="password" v-model="password" placeholder="Password">
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // Implement login logic here
+    }
+  }
+};
+</script>

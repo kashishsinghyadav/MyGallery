d58d496bb8c3776837f2ccbf3435d15d55c9357b
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-04-17 22:55:27 +0530

    Create index.js

---
+++ router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../components/Login.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+export default router;

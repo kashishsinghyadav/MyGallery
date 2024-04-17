4384519f68bf4404ce7f84856a1780820b93fcf7
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-04-17 22:54:44 +0530

    Create index.js

---
+++ store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    token: null
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+    clearToken(state) {
+      state.token = null;
+    }
+  },
+  actions: {
+    login({ commit }, { token }) {
+      commit('setToken', token);
+    },
+    logout({ commit }) {
+      commit('clearToken');
+    }
+  },
+  getters: {
+    isAuthenticated(state) {
+      return state.token !== null;
+    }
+  }
+});
